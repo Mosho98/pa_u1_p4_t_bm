dbc3660347b6cc9c855334d6695dd9800c3deeb1
Bryan Mullo <bfmullo@uce.edu.ec>
2023-05-15 13:16:30 -0500

    Tarea 3

---
+++ src/main/java/com/example/demo/PaU1P4T3BmApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaU1P4T3BmApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaU1P4T3BmApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("TAREA 3");
+	}
+
+}
