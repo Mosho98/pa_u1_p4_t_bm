9601db267771be55dd35f0cac4f8a57e4c39b17e
Bryan Mullo <bfmullo@uce.edu.ec>
2023-05-15 13:22:39 -0500

    TAREA 3

--- src/main/java/com/example/demo/PaU1P4T3BmApplication.java
+++ src/main/java/com/example/demo/PaU1P4T3BmApplication.java
@@ -15,6 +15,7 @@ public class PaU1P4T3BmApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("TAREA 3");
+		System.out.println("CAMBIO REALIZADO");
 	}
 
 }
